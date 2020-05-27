e87e8cd5e6b8620d5183c5af0b897a3bd9e11cbb
area123 <kjs0713kjs@naver.com>
2020-05-27 21:44:33 +0900

    메인페이지 버거버튼 기능추가

--- public/js/app.js
+++ public/js/app.js
@@ -19283,6 +19283,21 @@ module.exports = function(module) {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+document.addEventListener('DOMContentLoaded', function () {
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(function (el) {
+      el.addEventListener('click', function () {
+        var target = el.dataset.target;
+        var $target = document.getElementById(target);
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <title>Laravel</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
     @yield('lib')
 </head>
 <body>
